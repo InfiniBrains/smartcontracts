88ba0da26d7f88fb5bffc94569fb49aacbbc295b
Alexandre Tolstenko <tolstenko@gmail.com>
2022-02-12 10:37:04 -0300

    feat: add new tests

--- contracts/FabricaDeGeniosCoin.sol
+++ contracts/FabricaDeGeniosCoin.sol
@@ -1,8 +1,10 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.11;
-
+pragma solidity ^0.8.9;
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
 contract FabricaDeGeniosCoin is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("Fabrica de Genios Coin", "FGC") {}
+    constructor() ERC20PresetMinterPauser("Fabrica de Genios Coin", "FGC") {
+        console.log("contract created");
+    }
 }
