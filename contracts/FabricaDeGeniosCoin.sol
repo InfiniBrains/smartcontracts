1ff39aab15aac442da2221c691d1f112cf059a9d
Alexandre Tolstenko <tolstenko@gmail.com>
2022-02-19 12:41:00 -0300

    feat: todo list to create an awesome erc20 contract

--- contracts/FabricaDeGeniosCoin.sol
+++ contracts/FabricaDeGeniosCoin.sol
@@ -1,10 +1,59 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
-contract FabricaDeGeniosCoin is ERC20PresetMinterPauser {
-    constructor() ERC20PresetMinterPauser("Fabrica de Genios Coin", "FGC") {
+contract FabricaDeGeniosCoin is ERC20PresetFixedSupply, AccessControlEnumerable {
+    using Address for address;
+//    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+
+    // todo: protect against creation of different dex usages
+    // 1. protect the token to be used before DEX listing
+    // 2. implement transactions fees
+    //    - create sinkholes ex.: burn(total supply approach, transfer approach), team(dev, mkt...), liquidity, lottery
+    //    - add whitelist for exceptions from fees
+    //    - add different fees on buy or sell dex transactions
+    //    - exclude liquidity fee or other fees from common transacions
+    //    - limit the total fee percent to increase investor protection
+    // 3. implement anti dump measures
+    //    - if someone wants to dumps a bunch of tokens add a costly fee
+    // 4. anti bot measures
+    //    - protect against multiple dex transacions in a small amout of time
+    //    - add temp-bans, perma-ban, suspiction detection, attention time window
+    // 5. protect against user misuse
+    //    - add withdraw fuctions to give back tokens or native coin for users that send it to the contract
+
+    constructor() ERC20PresetFixedSupply("Fabrica de Genios Coin", "FGC", 1000000000 * 10**decimals(), _msgSender()) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+//        _setupRole(PAUSER_ROLE, _msgSender());
+
         console.log("contract created");
+    }
+
+    function withdraw(address payable to, uint256 amount) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(to != address(0), "transfer to the zero address");
+        require(amount <= payable(address(this)).balance, "You are trying to withdraw more funds than available");
+        to.transfer(amount);
+    }
+
+    function withdrawERC20(
+        address tokenAddress,
+        address to,
+        uint256 amount
+    ) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(tokenAddress.isContract(), "ERC20 token address must be a contract");
+
+        IERC20 tokenContract = IERC20(tokenAddress);
+        require(
+            tokenContract.balanceOf(address(this)) >= amount,
+            "You are trying to withdraw more funds than available"
+        );
+
+        require(tokenContract.transfer(to, amount), "Fail on transfer");
     }
 }
--- test/FabricaDeGeniosCoin.test.ts
+++ test/FabricaDeGeniosCoin.test.ts
@@ -20,29 +20,28 @@ describe("Fabrica de Genios", function () {
         contract = await contract.deployed();
     });
 
-    it("Should mint to a different account", async function () {
-        // value before should be 0
-        expect((await contract.balanceOf(address1.address)).toString()).to.equal(BigNumber.from(0).toString());
-
-        // mint
-        await contract.mint(address1.address, 1);
-
-        // value after should be 1
-        expect((await contract.balanceOf(address1.address)).toString()).to.equal(BigNumber.from(1).toString());
-    });
-
-    it("General user should not be able to mint", async function () {
-        // value before should be 0
-        expect((await contract.balanceOf(address1.address)).toString()).to.equal(BigNumber.from(0).toString());
-
-        // should fail
-        await expect(contract.connect(address1).mint(address1.address,1)).to.be.revertedWith('ERC20PresetMinterPauser: must have minter role to mint');
-
-        expect((await contract.balanceOf(address1.address)).toString()).to.equal(BigNumber.from(0).toString());
-    });
-
-    it("Should have the correct name", async function () {
+    it("Should have the correct name and total supply", async function () {
         expect((await contract.name())).to.equal("Fabrica de Genios Coin");
         expect((await contract.symbol())).to.equal("FGC");
+        expect( ethers.utils.formatEther(await contract.balanceOf(owner.address))).to.equal("1000000000.0");
+    });
+
+    it("The admin should be able to withdraw funds that user transfered wrongly", async function () {
+        // expect(await address1.sendTransaction({to: contract.address, value: 1})).to.be.revertedWith("function selector was not recognized and there's no fallback nor receive function");
+        await contract.transfer(address1.address, 1);
+
+        // swap caller
+        contract = contract.connect(address1);
+
+        // execute transfer
+        await contract.transfer(contract.address, 1);
+
+        // @ts-ignore
+        await expect(contract.withdrawERC20(contract.address, owner.address, 1)).to.be.revertedWith("AccessControl: account " + address1.address.toLowerCase() + " is missing role 0x0000000000000000000000000000000000000000000000000000000000000000");
+
+        // swap caller
+        contract = contract.connect(owner);
+        await contract.withdrawERC20(contract.address, owner.address, 1);
+        await expect(contract.withdrawERC20(contract.address, owner.address, 1)).to.be.revertedWith("You are trying to withdraw more funds than available");
     });
 });
