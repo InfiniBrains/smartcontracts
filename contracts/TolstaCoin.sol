8d8102c176d8a8b18b57bdf161161deccc0969ea
junior.marques <junior.marques@deixanoazul.com.br>
2022-02-20 11:35:38 -0300

    Create Tolsta Coin transfer functions

--- contracts/TolstaCoin.sol
+++ contracts/TolstaCoin.sol
@@ -1,8 +1,45 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
-contract TolstaCoin is ERC20PresetFixedSupply {
-    constructor() ERC20PresetFixedSupply("TolstaCoin", "TC", 1000000000 * 10**decimals(), _msgSender()){ }
+
+contract TolstaCoin is ERC20PresetFixedSupply, AccessControlEnumerable {
+    using Address for address;
+    
+    constructor() ERC20PresetFixedSupply("Tolsta Coin", "TC", 1000000000 * 10**decimals(), _msgSender()){ 
+        // ADD ROLE ADIMIN
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+
+        console.log("contract created");
+    }
+
+    
+    function withdraw(address payable to, uint256 amount) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(to != address(0), "Transfer to the zero address");
+        require(amount <= payable(address(this)).balance, "You are trying to withdraw more funds than available");
+        to.transfer(amount);
+    }
+
+    function withdrawERC20(
+        address tokenAddress,
+        address to,
+        uint256 amount
+    ) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(tokenAddress.isContract() , "ERC20 token address must be a contract");
+
+        IERC20 tokenContract = IERC20(tokenAddress);
+        require(
+            tokenContract.balanceOf(address(this)) >= amount,
+            "You are trying to withdraw more funds than available"
+        );
+
+        require(tokenContract.transfer(to, amount), "Fail on transfer");
+    }
+
 }
--- test/TolstaCoin.test.ts
+++ test/TolstaCoin.test.ts
@@ -1,31 +1,31 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
-import {FabricaDeGeniosCoin, FabricaDeGeniosCoin__factory} from "../typechain";
+import {TolstaCoin, TolstaCoin__factory} from "../typechain";
 
-describe("Fabrica de Genios", function () {
+describe("Tolsta Coin", function () {
     let owner: SignerWithAddress;
     let address1: SignerWithAddress;
     let address2: SignerWithAddress;
-    let contract: FabricaDeGeniosCoin;
+    let contract: TolstaCoin;
 
     before(async function () {
         [owner, address1, address2] = await ethers.getSigners();
     });
 
     beforeEach(async function () {
-        let contractFactory = <FabricaDeGeniosCoin__factory>await ethers.getContractFactory("FabricaDeGeniosCoin");
+        let contractFactory = <TolstaCoin__factory>await ethers.getContractFactory("TolstaCoin");
         contract = await contractFactory.deploy();
         contract = await contract.deployed();
     });
 
-    it("Should have the correct name and total supply", async function () {
-        expect((await contract.name())).to.equal("Fabrica de Genios Coin");
-        expect((await contract.symbol())).to.equal("FGC");
-        expect( ethers.utils.formatEther(await contract.balanceOf(owner.address))).to.equal("1000000000.0");
+    it("Should have the correct name and total supply. Tolsta Coin.", async function () {
+        expect((await contract.name())).to.equal("Tolsta Coin");
+        expect((await contract.symbol())).to.equal("TC");
+        expect(ethers.utils.formatEther(await contract.balanceOf(owner.address))).to.equal("1000000000.0");
     });
 
-    it("The admin should be able to withdraw funds that user transfered wrongly", async function () {
+    it("The admin should be able to withdraw funds that user transfered wrongly. Tolsta Coin.", async function () {
         // expect(await address1.sendTransaction({to: contract.address, value: 1})).to.be.revertedWith("function selector was not recognized and there's no fallback nor receive function");
         await contract.transfer(address1.address, 1);
 
