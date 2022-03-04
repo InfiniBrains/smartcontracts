eb476d705e1dfa5e02e5ea5db84d537ee328713a
Artur Felipe Veloso <arturfdasveloso@gmail.com>
2022-03-04 12:30:51 -0300

    ArtuinoCoin comments organization

--- contracts/ArtuinoCoin.sol
+++ contracts/ArtuinoCoin.sol
@@ -9,47 +9,26 @@ import "@openzeppelin/contracts/utils/Address.sol";
 
 contract ArtuinoCoin is ERC20PresetFixedSupply, AccessControlEnumerable {
     using Address for address;
-//    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-
-    // todo: protect against creation of different dexes
-    // 1. protect the token to be used before DEX listing
-    //    - create a function that only unpause or unlock the token
-    //    - reason: we are going to use BNB funds from public sale as liquidity
-    // 2. implement transactions fees
-    //    - create sinkholes ex.: burn(total supply approach vs transfer to dead address approach), team(dev, mkt...), liquidity, lottery. Each one with specific address.
-    //    - add whitelist for exceptions from fees
-    //    - add different fees on buy or sell dex transactions
-    //    - exclude liquidity fee or other fees from common transacions
-    //    - limit the total fee percent to increase investor protection
-    // 3. implement anti dump measures
-    //    - if someone wants to dumps a bunch of tokens add a costly fee
-    //    - create a funcion the more it wants to dump, more it would be taken
-    // 4. anti bot measures
-    //    - protect against multiple dex transacions in a small amout of time
-    //    - add temp-bans, perma-ban, suspiction detection, awareness time window
-    // 5. protect against user misuse
-    //    - add withdraw fuctions to give back tokens or native coin for users that send it to the contract
-    //    - add native coin, ERC20, ERC721, ERC1155 withdraw functions.
-    // 6. increase transparency:
-    //    - the contract owner cannot set abusive taxes
-    //    - the contract owner cannot pause trading after dex listing
-    //    - every set function should emit log
-    //    - comment / document all functions
-    //    - each contract should be placed in one file. it is preferrable to import openzeppelin and uniswap code and not embed it into one sigle file
+    bool paused = false;
+    // bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    
+    // Criação do token utilizando o padrão ERC20
 
     constructor() ERC20PresetFixedSupply("Artuino Coin", "ARC", 1000000000 * 10**decimals(), _msgSender()) {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-//        _setupRole(PAUSER_ROLE, _msgSender());
+        // _setupRole(PAUSER_ROLE, _msgSender());
 
         console.log("contract created");
     }
 
+    // Transferência 
     function withdraw(address payable to, uint256 amount) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
         require(to != address(0), "transfer to the zero address");
         require(amount <= payable(address(this)).balance, "You are trying to withdraw more funds than available");
         to.transfer(amount);
     }
 
+    // Transferência de token
     function withdrawERC20(
         address tokenAddress,
         address to,
@@ -65,4 +44,44 @@ contract ArtuinoCoin is ERC20PresetFixedSupply, AccessControlEnumerable {
 
         require(tokenContract.transfer(to, amount), "Fail on transfer");
     }
+
+
+    // Protect against creation of different dexes
+    
+    // 1. Protect the token to be used before DEX listing
+    // Reason: I assume we are going to use some BNB funds from crowdsale as liquidity
+
+    // 1.1 Create a function that only unpause or unlock the token
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override(ERC20) {
+        super._beforeTokenTransfer(from, to, amount);
+        require(!paused, "The wallet is paused");
+    }
+
+    // 1.2 Protect against creation of different pairs. corner case
+    // Cryptocurrencies (moedas base) BTC, ETH, USD
+    
+    // 1.3 Create a pair at the moment the coin is constructed
+
+
+    // 2. Implement transactions fees
+    // Reason: 
+    // 2.1 Create sinkholes ex.: burn(total supply approach vs transfer to dead address approach), team(dev, mkt...), liquidity, lottery. Each one with a specific address. - Partially done, only marketing is used.
+    // 2.2 Add whitelist for exceptions from fees
+    // 2.3 Add different fees on buy or sell dex transactions
+    // 2.4 Exclude liquidity fee or other fees from common transactions
+    // 2.5 Limit the total fee percent to increase investor protection
+    // 2.6 Reflection fee
+
+
+
+
+
+
+
+
+
 }
