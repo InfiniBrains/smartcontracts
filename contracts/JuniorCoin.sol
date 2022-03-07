7e34d8310131c382d179c55ce4d29c29b33072d5
junioralcant <junioralcant111@gmail.com>
2022-03-07 09:50:32 -0300

    Create withdraw functions in JuniorCoin

--- contracts/JuniorCoin.sol
+++ contracts/JuniorCoin.sol
@@ -1,8 +1,39 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.9;
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
-contract JuniorCoin is ERC20PresetFixedSupply {
-    constructor() ERC20PresetFixedSupply("JuniorCoin", "JRC", 1000000000 * 10**decimals(), _msgSender()){ }
+contract JuniorCoin is ERC20PresetFixedSupply, AccessControlEnumerable {
+    using Address for address;
+
+    constructor() ERC20PresetFixedSupply("JuniorCoin", "JRC", 1000000000 * 10**decimals(), _msgSender()){
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+     }
+
+     function withdraw(address payable to, uint256 amount) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(to != address(0), "transfer to the zero address");
+        require(amount <= payable(address(this)).balance, "You are trying to withdraw more funds than available");
+        to.transfer(amount);
+    }
+
+    function withdrawERC20(
+        address tokenAddress,
+        address to,
+        uint256 amount
+    ) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+        require(tokenAddress.isContract(), "ERC20 token address must be a contract");
+
+        IERC20 tokenContract = IERC20(tokenAddress);
+        require(
+            tokenContract.balanceOf(address(this)) >= amount,
+            "You are trying to withdraw more funds than available"
+        );
+
+        require(tokenContract.transfer(to, amount), "Fail on transfer");
+    }
 }
