7f911cbf1313329d6b064b3f0239f12a6df7b73f
junioralcant <junioralcant111@gmail.com>
2022-03-07 09:50:32 -0300

    Create withdraw functions in JuniorCoin

--- test/JuniorCoin.test.ts
+++ test/JuniorCoin.test.ts
@@ -25,22 +25,22 @@ describe("JuniorCoin", function () {
         expect( ethers.utils.formatEther(await contract.balanceOf(owner.address))).to.equal("1000000000.0");
     });
 
-    // it("The admin should be able to withdraw funds that user transfered wrongly, JuniorCoin", async function () {
-    //     // expect(await address1.sendTransaction({to: contract.address, value: 1})).to.be.revertedWith("function selector was not recognized and there's no fallback nor receive function");
-    //     await contract.transfer(address1.address, 1);
+    it("The admin should be able to withdraw funds that user transfered wrongly, JuniorCoin", async function () {
+        // expect(await address1.sendTransaction({to: contract.address, value: 1})).to.be.revertedWith("function selector was not recognized and there's no fallback nor receive function");
+        await contract.transfer(address1.address, 1);
 
-    //     // swap caller
-    //     contract = contract.connect(address1);
+        // swap caller
+        contract = contract.connect(address1);
 
-    //     // execute transfer
-    //     await contract.transfer(contract.address, 1);
+        // execute transfer
+        await contract.transfer(contract.address, 1);
 
-    //     // @ts-ignore
-    //     await expect(contract.withdrawERC20(contract.address, owner.address, 1)).to.be.revertedWith("AccessControl: account " + address1.address.toLowerCase() + " is missing role 0x0000000000000000000000000000000000000000000000000000000000000000");
+        // @ts-ignore
+        await expect(contract.withdrawERC20(contract.address, owner.address, 1)).to.be.revertedWith("AccessControl: account " + address1.address.toLowerCase() + " is missing role 0x0000000000000000000000000000000000000000000000000000000000000000");
 
-    //     // swap caller
-    //     contract = contract.connect(owner);
-    //     await contract.withdrawERC20(contract.address, owner.address, 1);
-    //     await expect(contract.withdrawERC20(contract.address, owner.address, 1)).to.be.revertedWith("You are trying to withdraw more funds than available");
-    // });
+        // swap caller
+        contract = contract.connect(owner);
+        await contract.withdrawERC20(contract.address, owner.address, 1);
+        await expect(contract.withdrawERC20(contract.address, owner.address, 1)).to.be.revertedWith("You are trying to withdraw more funds than available");
+    });
 });
