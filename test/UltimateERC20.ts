2fe1b2bb4c96cb90ce34f5ea6c7a85f4fb1cada7
Alexandre Tolstenko <tolstenko@gmail.com>
2022-03-18 17:17:43 -0300

    feat: testing with network fork

--- test/UltimateERC20.ts
+++ test/UltimateERC20.ts
@@ -1,45 +1,53 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import {UltimateERC20, UltimateERC20__factory} from "../typechain";
-import {SignerWithAddress} from "@nomiclabs/hardhat-ethers/signers";
+import { UltimateERC20, UltimateERC20__factory } from "../typechain";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
-describe("BetCoin", function () {
-    const DEAD_ADDRESS = "0x000000000000000000000000000000000000dEaD";
-    let contract: UltimateERC20;
-    let owner: SignerWithAddress;
-    let address1: SignerWithAddress;
-    let address2: SignerWithAddress;
-    let address3: SignerWithAddress;
-    let address4: SignerWithAddress;
-    let address5: SignerWithAddress;
+describe("UltimateCoin", function () {
+  const DEAD_ADDRESS = "0x000000000000000000000000000000000000dEaD";
+  let contract: UltimateERC20;
+  let owner: SignerWithAddress;
+  let address1: SignerWithAddress;
+  let address2: SignerWithAddress;
+  let address3: SignerWithAddress;
+  let address4: SignerWithAddress;
+  let address5: SignerWithAddress;
 
-    before(async function () {
-        [owner, address1, address2, address3, address4, address5] = await ethers.getSigners();
-    });
+  before(async function () {
+    [owner, address1, address2, address3, address4, address5] =
+      await ethers.getSigners();
+  });
 
-    beforeEach(async function () {
-        const betcoinFactory = <UltimateERC20__factory>await ethers.getContractFactory("UltimateERC20");
-        contract = await betcoinFactory.deploy("Test", "TST");
-        contract = await contract.deployed();
+  beforeEach(async function () {
+    const ultimateFactory = <UltimateERC20__factory>(
+      await ethers.getContractFactory("UltimateERC20")
+    );
+    contract = await ultimateFactory.deploy("Test", "TST");
+    contract = await contract.deployed();
 
-        // await contract.settransform(true);
-        // await contract.setEnableContract(true);
-        // await contract.setTeamWallet(address1.address);
-        // await contract.setLotteryWallet(address2.address);
-    });
+    // await contract.settransform(true);
+    // await contract.setEnableContract(true);
+    // await contract.setTeamWallet(address1.address);
+    // await contract.setLotteryWallet(address2.address);
+  });
 
-    // it("Should be able to withdraw funds", async function () {
-    //     const Greeter = await ethers.getContractFactory("Greeter");
-    //     const greeter = await Greeter.deploy("Hello, world!");
-    //     await greeter.deployed();
-    //
-    //     expect(await greeter.greet()).to.equal("Hello, world!");
-    //
-    //     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-    //
-    //     // wait until the transaction is mined
-    //     await setGreetingTx.wait();
-    //
-    //     expect(await greeter.greet()).to.equal("Hola, mundo!");
-    // });
+  it("Should have correct names", async function () {
+    expect(await contract.name()).to.equal("Test");
+    expect(await contract.symbol()).to.equal("TST");
+  });
+
+  // it("Should be able to withdraw funds", async function () {
+  //     const Greeter = await ethers.getContractFactory("Greeter");
+  //     const greeter = await Greeter.deploy("Hello, world!");
+  //     await greeter.deployed();
+  //
+  //     expect(await greeter.greet()).to.equal("Hello, world!");
+  //
+  //     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+  //
+  //     // wait until the transaction is mined
+  //     await setGreetingTx.wait();
+  //
+  //     expect(await greeter.greet()).to.equal("Hola, mundo!");
+  // });
 });
