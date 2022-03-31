b658b1352f2bb8b4e4b7f9881f3a7095f49131fb
seczar <milseg.dev@gmail.com>
2022-03-31 02:12:24 -0300

    Start busd liquidity

--- test/ERC20_FLiq_FEco_FBurn_AntiDump_DexTempBan.test.ts
+++ test/ERC20_FLiq_FEco_FBurn_AntiDump_DexTempBan.test.ts
@@ -5,14 +5,19 @@ import { ethers, network, waffle } from "hardhat";
 import { ERC20FLiqFEcoFBurnAntiDumpDexTempBan, ERC20FLiqFEcoFBurnAntiDumpDexTempBan__factory } from "../typechain";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { Contract, utils } from "ethers";
-import { expandTo9Decimals } from "./shared/utilities";
+import { expandTo9Decimals, expandTo18Decimals } from "./shared/utilities";
 import { abi } from "@uniswap/v2-periphery/build/UniswapV2Router02.json";
+import busdAbi from "./shared/busd.json";
 
 // ATTENTION! do not commit the line below!! You should put only for your tests only!
 //describe.only("ERC20FLiqFEcoFBurnAntiDumpDexTempBan", function () {
 describe("ERC20FLiqFEcoFBurnAntiDumpDexTempBan", function () {
     const DEAD_ADDRESS = "0x000000000000000000000000000000000000dEaD";
+    const BUSD_ADDRESS = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56";
     let contract: ERC20FLiqFEcoFBurnAntiDumpDexTempBan;
+    let busdContract: Contract;
+    let busdHotWalletAddress: string = '0x8894e0a0c962cb723c1976a4421c95949be2d4e3';
+    let busdHotWallet: SignerWithAddress;
     let owner: SignerWithAddress;
     let address1: SignerWithAddress;
     let address2: SignerWithAddress;
@@ -23,25 +28,34 @@ describe("ERC20FLiqFEcoFBurnAntiDumpDexTempBan", function () {
     before(async function () {
         [owner, address1, address2, address3, address4, address5] =
         await ethers.getSigners();
+
+        await network.provider.request({
+          method: "hardhat_impersonateAccount",
+          params: [busdHotWalletAddress],
+        });
+        busdHotWallet = await ethers.getSigner(busdHotWalletAddress);
+
+        busdContract = new ethers.Contract(BUSD_ADDRESS, busdAbi, busdHotWallet);
     });
 
     beforeEach(async function () {
         const ERC20Factory = <ERC20FLiqFEcoFBurnAntiDumpDexTempBan__factory>(
             await ethers.getContractFactory("ERC20FLiqFEcoFBurnAntiDumpDexTempBan")
         );
-        contract = await ERC20Factory.deploy("Test", "ERT", 1000000000);
+        contract = await ERC20Factory.deploy("Test", "ERT", expandTo18Decimals(1000000000));
         contract = await contract.deployed();
     });
 
     it("Should be constructed properly", async function () {
+        let totalSupply = await contract.totalSupply()
         expect(await contract.name()).to.equal("Test");
         expect(await contract.symbol()).to.equal("ERT");
-        expect(await contract.totalSupply()).to.equal(
-            utils.parseUnits("1000000000", 0).toString()
+        expect(totalSupply).to.equal(
+            expandTo18Decimals(1000000000).toString()
         );
         expect(await contract.decimals()).to.equal(18);
         expect(await contract.balanceOf(owner.address)).to.equal(
-            await contract.totalSupply()
+            totalSupply
         );
 
         expect(
@@ -51,10 +65,13 @@ describe("ERC20FLiqFEcoFBurnAntiDumpDexTempBan", function () {
         expect(await contract.isExcludedFromFees(await contract.owner())).to.equal(
         true
         );
+
+        console.log("totalSupply:", totalSupply);
     });
 
     it("Should be able to create a new pair", async function () {
-        const tx = await contract.addNewPair(
+        //IT HAS ALREADY BEEN CREATED ON CONSTRUCTOR
+        /*const tx = await contract.addNewPair(
             "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
         ); // BUSD address
         const receipt = await tx.wait();
@@ -63,9 +80,9 @@ describe("ERC20FLiqFEcoFBurnAntiDumpDexTempBan", function () {
             receipt.logs[2].topics[2]
         )[0];
 
-        expect(await contract.automatedMarketMakerPairs(newPair)).to.equal(true);
+        expect(await contract.automatedMarketMakerPairs(newPair)).to.equal(true);*/
         await expect(
-            contract.addNewPair("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56")
+            contract.addNewPair(BUSD_ADDRESS)
         ).to.be.revertedWith("Pancake: PAIR_EXISTS"); // should fail
     });
 
@@ -143,6 +160,16 @@ describe("ERC20FLiqFEcoFBurnAntiDumpDexTempBan", function () {
     it("Automated Market Maker Pair", async function () {
 
         expect(await contract.setAutomatedMarketMakerPair(DEAD_ADDRESS, true));
+    });
+
+    it("should start busd liquidity", async function () {
+        await busdContract
+                .connect(busdHotWallet)
+                .transfer(owner.address, expandTo18Decimals(1000000));
+
+        const balanceBusdOwner = await busdContract.balanceOf(owner.address);
+        console.log("balanceBusdOwner", balanceBusdOwner);
+        expect(balanceBusdOwner).to.equal(expandTo18Decimals(1000000));
     });
 
     // procurar como adicionar BUSD para cá
