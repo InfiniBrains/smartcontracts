2b1560f7cdb81011674908d9f5dc877327289d24
Alexandre Tolstenko <tolstenko@gmail.com>
2022-04-09 18:27:07 -0300

    feat: add time and price

--- contracts/Heap.sol
+++ contracts/Heap.sol
@@ -7,74 +7,81 @@ pragma solidity ^0.8.9;
 // Original Author: Zac Mitton
 // License: MIT
 // ref.: https://github.com/zmitton/eth-heap/blob/master/contracts/Heap.sol
+// ref.: https://github.com/zmitton/eth-heap/blob/master/contracts/OrderBookHeap.sol
+
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+// todo: get heap node by time
+// todo: remove nodes older than a provided time
 
 library Heap { // default max-heap
-  uint constant ROOT_INDEX = 1;
+  using SafeMath for uint256;
 
-  struct Data {
-    int128 idCount;
+  uint256 constant ROOT_INDEX = 1;
+
+  struct Data{
+    uint256 idCount;
     Node[] nodes; // root is index 1; index 0 not used
-    mapping (int128 => uint) indices; // unique id => node index
+    mapping (uint256 => uint256) indices; // unique time => node index
   }
   struct Node{
-    int128 id; //use with another mapping to store arbitrary object types
-    int128 priority;
+    uint256 id;
+    uint256 price;
+    uint256 time;
   }
 
   //call init before anything else
   function init(Data storage self) internal{
-    if(self.nodes.length == 0) self.nodes.push(Node(0,0));
+    if(self.nodes.length == 0) self.nodes.push(Node(0,0,0));
   }
 
-  function insert(Data storage self, int128 priority) internal returns(Node memory){//√
+  function insert(Data storage self, uint256 price, uint256 time) internal returns(Node memory){//√
     if(self.nodes.length == 0){ init(self); }// test on-the-fly-init
     self.idCount++;
-//    self.nodes.length++; // this is the old approach.
-    self.nodes.push(Node(0,0)); // todo: check if it is really working
-
-    Node memory n = Node(self.idCount, priority);
+//    self.nodes.length++; // old approach
+    self.nodes.push(Node(0,0,0));
+    Node memory n = Node(self.idCount, price, time);
     _bubbleUp(self, n, self.nodes.length-1);
     return n;
   }
   function extractMax(Data storage self) internal returns(Node memory){//√
     return _extract(self, ROOT_INDEX);
   }
-  function extractById(Data storage self, int128 id) internal returns(Node memory){//√
+  function extractById(Data storage self, uint256 id) internal returns(Node memory){//√
     return _extract(self, self.indices[id]);
   }
 
   //view
   function dump(Data storage self) internal view returns(Node[] memory){
-    //note: Empty set will return `[Node(0,0)]`. uninitialized will return `[]`.
+    //note: Empty set will return `[Node(0,0,0,0)]`. uninitialized will return `[]`.
     return self.nodes;
   }
-  // todo: check all the storage and memory usage. Probably we will edit the values of a given node
-  function getById(Data storage self, int128 id) internal view returns(Node storage){
+  function getById(Data storage self, uint256 id) internal view returns(Node storage){
     return getByIndex(self, self.indices[id]);//test that all these return the emptyNode
   }
-  function getByIndex(Data storage self, uint i) internal view returns(Node storage){
-//    return self.nodes.length > i ? self.nodes[i] : Node(0,0); // old approach
-    require(self.nodes.length > i, "Invalid index"); // todo: test this
+  function getByIndex(Data storage self, uint256 i) internal view returns(Node storage) {
+    require(self.nodes.length > i, "index not found");
+    // return self.nodes.length > i ? self.nodes[i] : Node(0,0,0,0); // old approach
     return self.nodes[i];
   }
   function getMax(Data storage self) internal view returns(Node storage){
     return getByIndex(self, ROOT_INDEX);
   }
-  function size(Data storage self) internal view returns(uint){
-    return self.nodes.length > 0 ? self.nodes.length-1 : 0;
+  function size(Data storage self) internal view returns(uint256){
+    return self.nodes.length > 0 ? self.nodes.length-1 : 0; // todo: is it really possible to be negative???
   }
   function isNode(Node memory n) internal pure returns(bool){ return n.id > 0; }
 
   //private
-  function _extract(Data storage self, uint i) private returns(Node memory){//√
-    if(self.nodes.length <= i || i <= 0){ return Node(0,0); }
+  function _extract(Data storage self, uint256 i) private returns(Node memory){//√
+    if(self.nodes.length <= i || i <= 0){ return Node(0,0,0); }
 
     Node memory extractedNode = self.nodes[i];
     delete self.indices[extractedNode.id];
 
     Node memory tailNode = self.nodes[self.nodes.length-1];
-//    self.nodes.length--; // old approach
-    self.nodes.pop(); // todo: test this
+    // self.nodes.length--; // old approach
+    self.nodes.pop(); // updated approach todo: check this
 
     if(i < self.nodes.length){ // if extracted node was not tail
       _bubbleUp(self, tailNode, i);
@@ -82,28 +89,28 @@ library Heap { // default max-heap
     }
     return extractedNode;
   }
-  function _bubbleUp(Data storage self, Node memory n, uint i) private{//√
-    if(i==ROOT_INDEX || n.priority <= self.nodes[i/2].priority){
+  function _bubbleUp(Data storage self, Node memory n, uint256 i) private{//√
+    if(i==ROOT_INDEX || n.price <= self.nodes[i/2].price){
       _insert(self, n, i);
     }else{
       _insert(self, self.nodes[i/2], i);
       _bubbleUp(self, n, i/2);
     }
   }
-  function _bubbleDown(Data storage self, Node memory n, uint i) private{//
-    uint length = self.nodes.length;
-    uint cIndex = i*2; // left child index
+  function _bubbleDown(Data storage self, Node memory n, uint256 i) private{//
+    uint256 length = self.nodes.length;
+    uint256 cIndex = i*2; // left child index
 
     if(length <= cIndex){
       _insert(self, n, i);
     }else{
       Node memory largestChild = self.nodes[cIndex];
 
-      if(length > cIndex+1 && self.nodes[cIndex+1].priority > largestChild.priority ){
-        largestChild = self.nodes[++cIndex];// TEST ++ gets executed first here
+      if(length > cIndex+1 && self.nodes[cIndex+1].price > largestChild.price ){
+        largestChild = self.nodes[++cIndex];
       }
 
-      if(largestChild.priority <= n.priority){ //TEST: priority 0 is valid! negative ints work
+      if(largestChild.price <= n.price){
         _insert(self, n, i);
       }else{
         _insert(self, largestChild, i);
@@ -112,7 +119,8 @@ library Heap { // default max-heap
     }
   }
 
-  function _insert(Data storage self, Node memory n, uint i) private{//√
+  // todo: check all functions using id
+  function _insert(Data storage self, Node memory n, uint256 i) private{//√
     self.nodes[i] = n;
     self.indices[n.id] = i;
   }
